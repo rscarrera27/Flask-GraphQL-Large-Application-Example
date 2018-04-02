4f2eca8df389914b739894b028edb66e59b60685
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-04-02 10:49:47 +0900

    Define Role Schema

--- GraphQL_SQLAlchemy/schema.py
+++ GraphQL_SQLAlchemy/schema.py
@@ -1,12 +1,18 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from GraphQL_SQLAlchemy.model import db_session, Department as DepartmentModel, Employee as EmployeeModel
+from GraphQL_SQLAlchemy.model import db_session, Department as DepartmentModel, Employee as EmployeeModel, Role as RoleModel
 
 
 class Department(SQLAlchemyObjectType):
     class Meta:
         model = DepartmentModel
+        interfaces = (relay.Node, )
+
+
+class Role(SQLAlchemyObjectType):
+    class Meta:
+        model = RoleModel
         interfaces = (relay.Node, )
 
 
