6941793e3a23baca3b7d0650f757a940444baa79
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-04-02 10:50:14 +0900

    Update DB initializer for Role

--- GraphQL_SQLAlchemy/database.py
+++ GraphQL_SQLAlchemy/database.py
@@ -1,4 +1,4 @@
-from GraphQL_SQLAlchemy.model import engine, db_session, Base, Department, Employee
+from GraphQL_SQLAlchemy.model import engine, db_session, Base, Department, Employee, Role
 
 Base.metadata.create_all(bind=engine)
 
@@ -6,6 +6,11 @@ engineering = Department(name='Engineering')
 hr = Department(name='Human Resources')
 
 [db_session.add(dpt) for dpt in [engineering, hr]]
+
+engineer = Role(name='Engineer')
+manager = Role(name='Manager')
+
+[db_session.add(role) for role in [engineer, manager]]
 
 peter = Employee(name='Peter', department=engineering)
 roy = Employee(name='Roy', department=engineering)
