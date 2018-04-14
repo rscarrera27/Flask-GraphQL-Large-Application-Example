1c9348708ad13960dfb23853f5e9a8f0de2f254c
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-04-14 23:42:12 +0900

    Update mutationas

--- GraphQL_MongoEngine/schema.py
+++ GraphQL_MongoEngine/schema.py
@@ -1,9 +1,13 @@
+import logging
 import graphene
-from graphene.relay import Node
-from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
+import trafaret as t
+from mongoengine import *
+from flask import *
 from model import Department as DepartmentModel
 from model import Employee as EmployeeModel
 from model import Role as RoleModel
+
+logger = logging.getLogger(__package__)
 
 
 def construct(object_type, mongo_obj):
@@ -12,7 +16,7 @@ def construct(object_type, mongo_obj):
     :param mongo_obj: mongoengine object
     :return: GraphQL Field that _id field changed to id
     """
-    field_names = [f.attname for f in object_type._meta_fields]
+    field_names = [f.attname for f in object_type._meta.fields]
     if 'id' in field_names:
         field_names.append('_id')
     kwargs = {attr:val for attr, val in mongo_obj.to_mongo().items() if attr in field_names}
@@ -36,40 +40,89 @@ class EmployeeField(graphene.ObjectType):
     id = graphene.String()
     name = graphene.String()
     hired_on = graphene.DateTime()
+    department = graphene.List(DepartmentField)
+    role = graphene.List(RoleField)
+
+
+class DepartmentMutation(graphene.Mutation):
+
+    class Input(object):
+        name = graphene.String()
+
+    department = graphene.Field(DepartmentField)
+
+    @classmethod
+    def mutate(cls, _, info, __):
+        logger.debug("args {0}".format(info))
+        dpt_schema = t.Dict({
+            'name': t.String(min_length=1)
+        })
+
+        dpt_data = dpt_schema.check(info)
+        department = DepartmentModel.objects.create(**dpt_data)
+        department.save()
+
+        return cls(user=construct(DepartmentField, department))
+
+
+class RoleMutation(graphene.Mutation):
+
+    class Input(object):
+        name = graphene.String()
+
+    role = graphene.Field(RoleField)
+
+    @classmethod
+    def mutate(cls, _, info, __):
+        logger.debug("args {0}".format(info))
+        role_schema = t.Dict({
+            'name': t.String(min_length=1)
+        })
+
+        role_data = role_schema.check(info)
+        role = RoleModel.objects.create(**role_data)
+        role.save()
+
+        return cls(user=construct(RoleField, role))
+
+
+class EmployeeMutation(graphene.Mutation):
+
+    class Input(object):
+        name = graphene.String()
+        department = graphene.List(DepartmentField)
+        role = graphene.List(RoleField)
+
+    employee = graphene.Field(EmployeeField)
     department = graphene.Field(DepartmentField)
     role = graphene.Field(RoleField)
 
-# class Department(MongoengineObjectType):
-#
-#     class Meta:
-#         model = DepartmentModel
-#         interfaces = (Node, )
-#
-#
-# class Employee(MongoengineObjectType):
-#
-#     class Meta:
-#         model = EmployeeModel
-#         interfaces = (Node, )
-#
-#
-# class Role(MongoengineObjectType):
-#
-#     class Meta:
-#         model = RoleModel
-#         interfaces = (Node, )
-#
-#
-# class Query(graphene.ObjectType):
-#     node = Node.Field()
-#     all_employees = MongoengineConnectionField(Employee)
-#     all_role = MongoengineConnectionField(Role)
-#     all_Department = MongoengineConnectionField(Department)
-#
-#     employees = graphene.Field(Employee, name=graphene.String())
-#
-#     def resolve_employees(self, args, context, info):
-#         query = Employee.get_node()
+    @classmethod
+    def mutate(cls, _, info, __):
+        logger.debug("args {0}".format(info))
+        employee_schema = t.Dict({
+            'name': t.String(min_length=2),
+            'department': t.String(min_length=2),
+            'role': t.String(min_length=2)
+        })
 
+        employee_data = employee_schema.check(info)
+        department_name = employee_data.pop('department')
+        role_name = employee_data.pop('role')
 
-schema = graphene.Schema(query=Query, types=[Department, Employee, Role])
+        try:
+            department = DepartmentModel.objects.get(name=department_name)
+            role = RoleModel.objects.get(name=role_name)
+
+            employee_data = employee_data.update({
+                'department': department,
+                'role': role
+            })
+
+            employee = EmployeeModel(**employee_data)
+            employee.save()
+
+            return cls(EmployeeField, employee)
+
+        except (MultipleObjectsReturned, DoesNotExist, ValidationError):
+            abort(404)
