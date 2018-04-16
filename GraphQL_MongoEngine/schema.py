2b7f668bada95568dd593d6172c996dd9485f48c
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-04-16 22:50:37 +0900

    Fix query's resolve methods Bugs

--- GraphQL_MongoEngine/schema.py
+++ GraphQL_MongoEngine/schema.py
@@ -16,12 +16,15 @@ def construct(object_type, mongo_obj):
     :param mongo_obj: mongoengine object
     :return: GraphQL Field that _id field changed to id
     """
-    field_names = [f.attname for f in object_type._meta.fields]
+    field_names = list(object_type._meta.fields)
+    print("line 21: {}".format(field_names))
     if 'id' in field_names:
         field_names.append('_id')
-    kwargs = {attr:val for attr, val in mongo_obj.to_mongo().items() if attr in field_names}
+    print("line 24: ".format(mongo_obj))
+    kwargs = {attr: val for attr, val in mongo_obj.to_mongo().items() if attr in field_names}
     if '_id' in kwargs:
-        kwargs['id'] = kwargs.pop('_id')
+        kwargs['id'] = str(kwargs.pop('_id'))
+    print("ine 27: {}".format(kwargs))
 
     return object_type(**kwargs)
 
@@ -40,89 +43,83 @@ class EmployeeField(graphene.ObjectType):
     id = graphene.String()
     name = graphene.String()
     hired_on = graphene.DateTime()
-    department = graphene.List(DepartmentField)
-    role = graphene.List(RoleField)
+    department = graphene.String()
+    role = graphene.String()
 
 
 class DepartmentMutation(graphene.Mutation):
 
-    class Input(object):
+    class Arguments(object):
         name = graphene.String()
 
     department = graphene.Field(DepartmentField)
 
     @classmethod
-    def mutate(cls, _, info, __):
-        logger.debug("args {0}".format(info))
-        dpt_schema = t.Dict({
-            'name': t.String(min_length=1)
+    def mutate(cls, info, _name):
+        print(_name)
+        name = dict({
+            "name": _name
         })
-
-        dpt_data = dpt_schema.check(info)
-        department = DepartmentModel.objects.create(**dpt_data)
+        department = DepartmentModel.objects(**name)
         department.save()
 
-        return cls(user=construct(DepartmentField, department))
+        return cls(department=construct(DepartmentField, department))
 
 
 class RoleMutation(graphene.Mutation):
 
-    class Input(object):
+    class Arguments(object):
         name = graphene.String()
 
     role = graphene.Field(RoleField)
 
     @classmethod
-    def mutate(cls, _, info, __):
-        logger.debug("args {0}".format(info))
-        role_schema = t.Dict({
-            'name': t.String(min_length=1)
-        })
-
-        role_data = role_schema.check(info)
+    def mutate(cls, info, name):
+        print(name)
+        role_data = name
+        print(role_data)
         role = RoleModel.objects.create(**role_data)
         role.save()
 
-        return cls(user=construct(RoleField, role))
+        return cls(role=construct(RoleField, role))
 
 
 class EmployeeMutation(graphene.Mutation):
 
-    class Input(object):
+    class Arguments(object):
         name = graphene.String()
-        department = graphene.List(DepartmentField)
-        role = graphene.List(RoleField)
+        department = graphene.String()
+        role = graphene.String()
 
     employee = graphene.Field(EmployeeField)
-    department = graphene.Field(DepartmentField)
-    role = graphene.Field(RoleField)
 
-    @classmethod
-    def mutate(cls, _, info, __):
-        logger.debug("args {0}".format(info))
-        employee_schema = t.Dict({
-            'name': t.String(min_length=2),
-            'department': t.String(min_length=2),
-            'role': t.String(min_length=2)
-        })
+    def mutate(self, info, name, department, role):
 
-        employee_data = employee_schema.check(info)
-        department_name = employee_data.pop('department')
-        role_name = employee_data.pop('role')
+        name = name
+        department_name = department
+        role_name = role
 
         try:
-            department = DepartmentModel.objects.get(name=department_name)
-            role = RoleModel.objects.get(name=role_name)
+            department = DepartmentModel.objects.get(name=department_name).name
+            role = RoleModel.objects.get(name=role_name).name
 
-            employee_data = employee_data.update({
+            print("line 112: {}".format(department))
+            print("line 113: {}".format(role))
+
+
+            employee_data = dict({
+                'name': name,
                 'department': department,
                 'role': role
             })
 
+            print("line 117: {}".format(employee_data))
+
             employee = EmployeeModel(**employee_data)
+            print("line 120: {}".format(employee.to_mongo().items()))
             employee.save()
 
-            return cls(EmployeeField, employee)
+            return EmployeeMutation(construct(EmployeeField, employee))
 
         except (MultipleObjectsReturned, DoesNotExist, ValidationError):
             abort(404)
@@ -130,44 +127,47 @@ class EmployeeMutation(graphene.Mutation):
 
 class Query(graphene.ObjectType):
 
-    department = graphene.Field(DepartmentField,
-                                description="Department Query",
-                                name=graphene.Argument(graphene.String))
+    department = graphene.List(of_type=DepartmentField,
+                               name=graphene.String(default_value="all"))
 
-    role = graphene.Field(RoleModel,
-                          description="Role Query",
-                          name=graphene.Argument(graphene.String))
-
-    employee = graphene.Field(EmployeeField,
-                              description="Employee Query",
-                              name=graphene.Argument(graphene.String),
-                              role=graphene.Argument(graphene.String),
-                              department=graphene.Argument(graphene.String))
+    role = graphene.List(of_type=RoleField,
+                         name=graphene.String(default_value="all"))
 
     hello = graphene.String(name=graphene.String(default_value="world"))
 
-    def resolve_department(self, args, info):
-        department = DepartmentModel.objects.get(name=args.get('department'))
+    def resolve_department(self, info, name):
+        if name == "all":
+            department = [construct(DepartmentField, object) for object in DepartmentModel.objects]
+            print("line 157: {}".format(department))
+            return department
+        else:
+            department = DepartmentModel.objects.get(name=name)
+            print("line 160: {}".format(DepartmentField))
+            return [construct(DepartmentField, department)]
 
-        return construct(DepartmentField, department)
+    def resolve_role(self, info, name):
+        if name == "all":
+            role = [construct(RoleField, object) for object in RoleModel.objects]
+            print("line 157: {}".format(role))
+            return role
+        else:
+            role = RoleModel.objects.get(name=name)
+            print("line 160: {}".format(role))
+            return [construct(RoleField, role)]
 
-    def resolve_role(self, args, info):
-        role = RoleModel.objects.get(name=args.get('role'))
+    def resolve_hello(self, info, name):
+        return 'Hello ' + name
 
-        return construct(RoleField, role)
-
-    def resolve_employee(self, args, info):
-        pass
 
 class Mutation(graphene.ObjectType):
-    create_employee = graphene.Field(EmployeeMutation,
-                                     description="Create New Employee")
+    create_employee = EmployeeMutation.Field()
 
-    create_role = graphene.Field(RoleMutation,
-                                 description="Create New Role")
+    create_role = RoleMutation.Field()
 
-    create_deaprtment = graphene.Field(DepartmentMutation,
-                                       description="Create New Department")
+    create_department = DepartmentMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
+
+# result = schema.execute('{ department{ name } }')
+# print(result.data)
