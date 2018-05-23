7ce8f0898febb0f03733af2df672abdb195e5bf6
devArtoria <contact@artoria.us>
2018-05-23 16:55:46 +0900

    UPDATE: add argument filter for query resolver

--- Server/app/graphql_view/view_v1/query.py
+++ Server/app/graphql_view/view_v1/query.py
@@ -4,7 +4,7 @@ from app.model.model_v1.DepartmentModel import DepartmentModel
 from app.model.model_v1.EmployeeModel import EmployeeModel
 from app.model.model_v1.RoleModel import RoleModel
 from app.graphql_view.view_v1.fields import DepartmentField, RoleField, EmployeeField
-from util.constructor import construct
+from util import construct, argument_filter
 
 
 class Query(graphene.ObjectType):
@@ -30,16 +30,17 @@ class Query(graphene.ObjectType):
             return [construct(DepartmentField, department)]
 
     def resolve_role(self, info, **kwargs):
-        name = kwargs["name"]
-        if name == "all":
+        query = argument_filter(kwargs)
+        if query["name"] == "all":
             role = [construct(RoleField, object) for object in RoleModel.objects]
             return role
         else:
-            role = RoleModel.objects.get(name=name)
+            role = RoleModel.objects.get(**query)
             return [construct(RoleField, role)]
 
     def resolve_employee(self, info, **kwargs):
         name = kwargs["name"]
+        query = argument_filter(kwargs)
         def make_employee(employee):
 
             department = DepartmentModel.objects.get(id=employee.department.id)
@@ -55,11 +56,11 @@ class Query(graphene.ObjectType):
 
             return employee
 
-        if name == "all":
+        if query["name"] == "all":
             employee = [make_employee(document) for document in EmployeeModel.objects]
             return employee
         else:
-            employee = EmployeeModel.objects.get(name=name)
+            employee = EmployeeModel.objects.get(**query)
             employee = make_employee(employee)
 
             return [employee]
