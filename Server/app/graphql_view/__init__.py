8deaf270e2656545f40353be9fd9e4083840e16b
devArtoria <contact@artoria.us>
2018-05-27 23:49:04 +0900

    PROTOTYPE: JWT auth decorator

--- Server/app/graphql_view/__init__.py
+++ Server/app/graphql_view/__init__.py
@@ -1,7 +1,25 @@
 import graphene
+from functools import wraps
+from flask_jwt_extended import get_jwt_identity, get_raw_jwt, jwt_required, jwt_refresh_token_required
 from flask_graphql import GraphQLView
 from app.graphql_view.view_v1.mutation import Mutation
 from app.graphql_view.view_v1.query import Query
+
+blacklist = set()
+
+
+def jwt_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def jwt_refresh_token_required(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapper
 
 
 class Schema:
