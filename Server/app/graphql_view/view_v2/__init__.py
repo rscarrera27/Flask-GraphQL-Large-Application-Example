cf3e7f1005d4a33e9adae49f8fdf0d7024a24bfb
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-06-10 23:51:27 +0900

    UPDATE: auth wrapper

--- Server/app/graphql_view/view_v2/__init__.py
+++ Server/app/graphql_view/view_v2/__init__.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask_graphql_auth import jwt_required, get_jwt_identity
+from flask_graphql_auth import jwt_required, get_jwt_identity, jwt_refresh_token_required
 
 from app.model.model_v2.account import AccountModel
 
@@ -13,13 +13,17 @@ def auth_required(fn):
     def wrapper(*args, **kwargs):
         account = AccountModel.objects(id=get_jwt_identity()).first()
         if account is None:
-            return None
+            return None, "Invalid access token"
         return fn(*args, **kwargs)
     return wrapper
 
 
 def refresh_required(fn):
     @wraps(fn)
+    @jwt_refresh_token_required
     def wrapper(*args, **kwargs):
-        return None
+        if get_jwt_identity() in blacklist:
+            return None, "Invalid refresh token"
+        else:
+            return fn(*args, **kwargs)
     return wrapper
