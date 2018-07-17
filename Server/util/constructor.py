bace6b019fd72069bf02034bfe709848a6dfaf42
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-17 20:23:19 +0900

    UPDATE: argument name

--- Server/util/constructor.py
+++ Server/util/constructor.py
@@ -1,16 +1,12 @@
-def construct(object_type, mongo_obj):
-    """
-    :param object_type: GraphQL Field class
-    :param mongo_obj: mongoengine object
-    :return: GraphQL Field that _id field changed to id
-    """
-    field_names = list(object_type._meta.fields)
+def construct(graphene_field, mongo_obj):
+    field_names = list(graphene_field._meta.fields)
 
     if 'id' in field_names:
         field_names.append('_id')
+
     kwargs = {attr: val for attr, val in mongo_obj.to_mongo().items() if attr in field_names}
 
     if '_id' in kwargs:
         kwargs['id'] = str(kwargs.pop('_id'))
 
-    return object_type(**kwargs)
+    return graphene_field(**kwargs)
