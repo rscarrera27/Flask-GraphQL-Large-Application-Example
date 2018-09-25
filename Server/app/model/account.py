8c2aea914f951f85fa2f16fbdb68d389221e072b
Lewis <artoria@artoria.us>
2018-09-25 16:27:19 +0900

    UPDATE: remove column

--- Server/app/model/account.py
+++ Server/app/model/account.py
@@ -7,5 +7,4 @@ class AccountModel(Document):
     id = StringField(required=True, primary_key=True)
     username = StringField(required=True)
     password = StringField(required=True)
-    description = StringField()
     register_on = DateTimeField(required=True, default=datetime.now())
