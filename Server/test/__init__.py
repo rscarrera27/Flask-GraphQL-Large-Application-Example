27d0ac7121a6e6405cb23d4812ba833fe299924a
Lewis <artoria@artoria.us>
2018-09-25 16:29:23 +0900

    UPDATE: base TC example

---
+++ Server/test/__init__.py
@@ -0,0 +1,57 @@
+from app.models import *
+import unittest
+from datetime import datetime, timedelta
+from config import dev
+from app import create_app
+import json
+
+app = create_app(dev.DevConfig)
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+        app.testing = True
+        self.tester = app.test_client(self)
+
+    def request(self, type, call, body):
+        query = type + " {" + call + "{" + body + "}" + "}"
+
+        response = self.tester.post('/graphql',
+                                    data=json.dumps({"query": query}),
+                                    content_type='application/json'
+                                    )
+
+        print(response.json)
+        return dict(response.json)['data']
+
+    def _create_fake_data(self):
+        # Create fake data
+        pass
+
+    def _get_tokens(self):
+        response = self.request(type="mutation",
+                                call='auth(id:"{0}", password:"{1}")'.format("fake user's id", "fake user's pw"),
+                                body='''
+                                     result{
+                                     ... on AuthField{
+                                     refreshToken
+                                     accessToken
+                                     message
+                                     }
+                                     }
+                                     ''')
+
+        response = response['auth']
+        self.access_token = response['result']['accessToken']
+        self.refresh_token = response['result']['refreshToken']
+
+    def setUp(self):
+        self._create_fake_data()
+        self._get_tokens()
+
+    def tearDown(self):
+        # teardown code ex. drop tables
+        pass
